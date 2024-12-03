4c814adfd7cef2d7f0c6c79c229b9fa8b9a9db47
Kaloyan Kolev <kaloyan_tihomirov@icloud.com>
2024-12-03 16:11:55 +0200

    added minimum consecutive cards to pick up from leetcode

---
+++ sliding-window-pattern/minConsecutiveCards.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+
+int minConsecutiveCards(const std::vector<size_t>& cards)
+{
+    size_t currWindowLen = 0;
+    size_t minLen = cards.size() + 1;
+
+    std::unordered_map<size_t, size_t> cardsCount;
+    for (size_t i = 0; i < cards.size(); i++)
+    {
+        cardsCount[cards[i]]++;
+        currWindowLen++;
+
+        while (cardsCount[cards[i]] > 1)
+        {
+            minLen = std::min(minLen, currWindowLen);
+            size_t firstCardInWindow = cards[i - (currWindowLen - 1)];
+            cardsCount[firstCardInWindow]--;
+            currWindowLen--;
+        }
+    }
+
+    return minLen == cards.size() + 1 ? -1 : minLen;
+}
+
+int main()
+{
+    std::cout << minConsecutiveCards(std::vector<size_t> { 3, 4, 2, 3, 4, 7 });
+}
--- sliding-window-pattern/readme.md
+++ sliding-window-pattern/readme.md
@@ -6,3 +6,4 @@ This folder contains solutions and explanations for problems solved using the **
 
 - [Minimum Size Subarray Sum](https://leetcode.com/problems/minimum-size-subarray-sum/description/)
 - [Longest Substring with At Most K Distinct Characters](https://leetcode.com/problems/longest-substring-with-at-most-k-distinct-characters/description/)
+- [Minimum Consecutive Cards to Pick Up](https://leetcode.com/problems/minimum-consecutive-cards-to-pick-up/description/)
