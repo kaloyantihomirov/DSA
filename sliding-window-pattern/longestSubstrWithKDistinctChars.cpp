d0ada0485ef1c97b1ca14d948f99d2fc236279da
Kaloyan Kolev <kaloyan_tihomirov@icloud.com>
2024-12-03 15:47:52 +0200

    improved longest substring len with at most k distinct characters solution

--- sliding-window-pattern/longestSubstrWithKDistinctChars.cpp
+++ sliding-window-pattern/longestSubstrWithKDistinctChars.cpp
@@ -1,43 +1,36 @@
-// longestSubstrWithKDistinctChars.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+/* Find the length of the longest substring with at most k distinct characters. */
 
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+
 using namespace std;
 
-void deleteLetters(unordered_map<char, int>& m, char l)
+size_t longestSubstringLenWithKDistinctCharacters(const std::string& s, size_t k)
 {
-    if (m[l] == 0)
-    {
-        m.erase(l);
-    }
-}
+    if (s.empty() || k == 0) return 0;
 
-int getLongestSubstrWithDistinctCharacters(string s, int k)
-{
-    int maxLen = 0;
-    int currentLen = 0;
-    unordered_map<char, int> m;
-    int left = 0;
-    for (int right = 0; right < s.size(); right++)
+    std::unordered_map<char, size_t> charsCount;
+    size_t maxLen = 0;
+    size_t currWindowLen = 0;
+    for (size_t i = 0; i < s.size(); i++)
     {
-        m[s[right]]++;
-        currentLen++;
+        charsCount[s[i]]++;
+        currWindowLen++;
 
-        if (m.size() <= k)
+        if (charsCount.size() <= k)
         {
-            maxLen = std::max(maxLen, currentLen);
+            maxLen = std::max(maxLen, currWindowLen);
         }
         else
         {
-            while (m.size() > k)
-            {
-                m[s[left]]--;
-                deleteLetters(m, s[left]);
-                left++;
-                currentLen--;
-            }
+            char charToRemove = s[i - (currWindowLen - 1)];
+	        while (charsCount[charToRemove] > 0)
+	        {
+                charsCount[charToRemove]--;
+                currWindowLen--;
+	        }
+            charsCount.erase(charToRemove);
         }
     }
 
@@ -46,16 +39,5 @@ int getLongestSubstrWithDistinctCharacters(string s, int k)
 
 int main()
 {
-    std::cout << getLongestSubstrWithDistinctCharacters("AAIBBBCCC", 3);
+    std::cout << longestSubstringLenWithKDistinctCharacters("AAAHHIBC", 2);
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
